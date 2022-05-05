4a31e410d780ac5c687956478ef17933f0863634
vsolaberrieta <vsolaberrieta@sophiagenetics.com>
2022-05-05 12:09:46 +0200

    req and ci

--- moves.py
+++ moves.py
@@ -1,5 +1,5 @@
 import re
-prog = re.compile("^((X|([NR][a-h]?x?|[NR][1-8]?x?|[a-hBQK]x)?[NBRQK]?[a-h][1-8]|([a-h]x)?[a-h][1-8][NBRQK]?|O-O|O-O-O)[\+#]?)$")
+prog = re.compile(r'^((X|([NR][a-h]?x?|[NR][1-8]?x?|[a-hBQK]x)?[NBRQK]?[a-h][1-8]|([a-h]x)?[a-h][1-8][NBRQK]?|O-O|O-O-O)[\+#]?)$')
 
 def is_valid_move(player_input):
     # print(player_input)
